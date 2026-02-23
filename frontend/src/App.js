f10eb6d6912e840df22b3d928645b5620fa55117
Liven Maharjan <np03cs4a220111@heraldcollege.edu.np>
2026-02-23 18:52:13 +0545

    Nav bar

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav />
+        <h1>Welcome to CarryMandu</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
